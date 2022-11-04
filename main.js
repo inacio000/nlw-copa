aaa0ce881ff8721d38182de4e6f640273e92f815
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2022-11-04 15:41:57 +0300

    upload team flags and update js file

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 function createGame(player1, hour, player2) {
   return `
     <li>
-      <img src="accets/icon-${player1}.svg" alt="${player1} Flag">
+      <img src="accets/Bandeiras-dos-países/icon=${player1}.svg" alt="${player1} Flag">
       <strong>${hour}</strong>
-      <img src="accets/icon-${player2}.svg" alt="${2} Flag">
+      <img src="accets/Bandeiras-dos-países/icon=${player2}.svg" alt="${2} Flag">
     </li>
   `
 }
 
-function createCard(date, day, games) {
+function createCard(date, day, group, games) {
   return `
     <div class="card">
-      <h2>${date}<span>${day}</span></h2>
+      <h2>${date}<span>${day} (Grupo ${group})</span></h2>
         <ul>
           ${games}
         </ul>
@@ -25,8 +25,13 @@ document.querySelector("#app").innerHTML = `
   </header>
 
   <main id="cards">
-    ${createCard('24/11', 'quinta', createGame('brazil', '16:00', 'serbia'))}
-    ${createCard('28/11', 'segunda', createGame('brazil', '13:00', 'switzerland'))}
-    ${createCard('02/12', 'sexta', createGame('cameroon', '16:00', 'brazil'))}
+    ${createCard("20/11", "Domingo", 'a', createGame("qatar", "19:00", "ecuador"))}
+    ${createCard(
+      "21/11",
+      "segunda", 'b & a',
+      createGame("england", "16:00", "iran") +
+        createGame("senegal", "19:00", "netherlands")
+    )}
+    ${createCard("21/11", "Segunda", 'B', createGame("united-states", "16:00", "wales"))}
   </main>
 `
